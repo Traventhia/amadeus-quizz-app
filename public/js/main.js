6887d42430a323dd8c0adf80bb0d7d907ce07190
d4lion <d4lion.dev@gmail.com>
2024-08-20 02:10:33 -0500

    :fire: feat: Handler game with questions request

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+// Encargado de los request a la API
+import { getQuestions } from './questions.js';
+
+// Variables globales
+import { API_URL } from './var.env.js';
+
+// Manejador del inicio del juego
+const startGame = init;
+
+// Preguntar por la API para usarla no se liberará en un archivo publico
+let dataApi = API_URL;
+
+// Boton de reinicio  del juego
+document.getElementById('replayButton').addEventListener('click', () => {
+  console.log('Reiniciando juego');
+  resetAnswers();
+  resetGame();
+});
+
+getQuestions(dataApi, (res) => {
+  questions = res;
+  console.log(res);
+  setTimeout(() => {
+    startGame();
+  }, 1000);
+});
